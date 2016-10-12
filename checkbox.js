64035af99272dbe8389f80c30a88c087beddcd3f
Maxime Pawlak <contact@maximepawlak.fr>
2016-10-12 14:56:26 +0200

    Add selection with :not(:checked)'

--- checkbox.js
+++ checkbox.js
@@ -5,10 +5,28 @@ console.log("Checkbox");
 $('#submitBtn').click(function(){
 	console.log('submit');
 
-	var checkGender = $("#checkGender");
+	var checkboxes = $("input[type='checkbox']:not(:checked)");
+	// var checkboxes = $("input:checkbox:not(:checked)");
+	console.log(checkboxes.length);
+
+	checkboxes.parent('div').hide();
+	// for(var i=0; i<checkboxes.length; i++){
+	// 	var checkbox = $(checkboxes[i]);
+	// 	var isChecked = checkbox.is(':checked');
+	// 	if(isChecked){
+
+	// 	}else{
+
+	// 	}
+	// }
+	
+});
+
+
+function level1(){
 	var isChecked = checkGender.is(':checked');
 	console.log("(jquery) Checkbox is checked ?", isChecked);
 
 	var isCheckedVanilla = document.getElementById('checkGender').checked;
-	console.log("(vanilla) Checkbox is checked ?", isCheckedVanilla);
-});
+	console.log("(vanilla) Checkbox is checked ?", isCheckedVanilla);	
+}
